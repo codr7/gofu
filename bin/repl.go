167286f796c3e717acdaa35e87f13bc4aa5781d6
Andreas Nilsson <andreas.nilsson@jayway.com>
2021-07-27 06:12:20 +0200

    todo

--- bin/repl.go
+++ bin/repl.go
@@ -18,7 +18,7 @@ func main() {
 	var thread gofu.TThread
 	thread.Init(&scope)
 
-	scope.BindSlot("stack", types.Stack(), thread.Stack())
+	scope.BindSlot("stack", types.Stack(types.Any()), thread.Stack())
 	
 	//p := gofu.Pos("repl", 1, 1)
 	block.Emit(ops.Push(types.Int(), 7))
