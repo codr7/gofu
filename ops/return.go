c5f02adfad0a3ad8cace6ceeb3bc4265cda65fba
Andreas Nilsson <andreas.nilsson@jayway.com>
2021-07-27 05:35:03 +0200

    Check returned stack.

--- ops/return.go
+++ ops/return.go
@@ -25,10 +25,14 @@ func (self TReturn) Eval(thread *gofu.TThread, pc *int) error {
 	f := c.Target().(*gofu.TFunc)
 	stack := thread.Stack();
 	offs := stack.Len()-1
+
+	if x, y := stack.Len(), f.ResCount(); x < y {
+		return fmt.Errorf("Invalid result: %v/%v/%v", f.Name(), x, y)
+	}
 	
 	for i, t := range f.ArgTypes() {
 		if v := stack.Peek(offs-i); !gofu.Isa(v.Type(), t) {
-			return fmt.Errorf("Invalid result: %v/%v", v, t.Name())
+			return fmt.Errorf("Invalid result: %v/%v/%v", f.Name(), v, t.Name())
 		}
 	}
 
