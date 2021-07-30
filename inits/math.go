12be0dd02df168210ffb474add2bce6e04c440ad
Andreas Nilsson <andreas.nilsson@jayway.com>
2021-07-30 04:22:09 +0200

    Simplify nested groups.

--- inits/math.go
+++ inits/math.go
@@ -11,7 +11,20 @@ func Math(scope *gofu.TScope) {
 		gofu.Func("+", []gofu.Type{types.Int(), types.Int()}, []gofu.Type{types.Int()},
 			func(pos gofu.TPos, thread *gofu.TThread, _func *gofu.TFunc, pc *int, check bool) error {
 				stack := thread.Stack()
-				stack.Push(types.Int(), stack.Pop().Value().(int) + stack.Pop().Value().(int))
+				y := stack.Pop()
+				x := stack.Peek(0)
+				x.Init(types.Int(), x.Value().(int) + y.Value().(int))
+				return nil
+			}))
+
+	scope.BindSlot("-",
+		types.Func(),
+		gofu.Func("-", []gofu.Type{types.Int(), types.Int()}, []gofu.Type{types.Int()},
+			func(pos gofu.TPos, thread *gofu.TThread, _func *gofu.TFunc, pc *int, check bool) error {
+				stack := thread.Stack()
+				y := stack.Pop()
+				x := stack.Peek(0)
+				x.Init(types.Int(), x.Value().(int) - y.Value().(int))
 				return nil
 			}))
 }
--- parsers/group.go
+++ parsers/group.go
@@ -16,11 +16,64 @@ func Group(memberParser gofu.Parser) *TGroup {
 	return &TGroup{memberParser: memberParser}
 }
 
-func (self TGroup) Form(pos *gofu.TPos, in *bufio.Reader) (gofu.Form, error) {
+func (self TGroup) Tail(fpos gofu.TPos, pos *gofu.TPos, in *bufio.Reader) (gofu.Form, error) {
 	var c rune
 	var f gofu.Form
-	var err error
 	var out []gofu.Form
+	var err error
+
+	for {
+		if c, _, err = in.ReadRune(); err == io.EOF {
+			return nil, errors.Parse(*pos, "Missing group end")
+		} else if err != nil {
+			return nil, err
+		}
+		
+		if c == ')' {
+			break
+		}
+
+		if c == ';' {
+			f, err = self.Tail(fpos, pos, in)
+
+			if err != nil {
+				if err == io.EOF {
+					return nil, errors.Parse(*pos, "Missing group end")
+				}
+
+				return nil, err
+			}
+
+			out = append(out, f)
+			break
+		} else {
+			in.UnreadRune()
+			f, err = self.memberParser.Form(pos, in)
+
+			if err != nil {
+				if err == io.EOF {
+					return nil, errors.Parse(*pos, "Missing group end")
+				}
+			}
+
+			if f == nil {
+				break
+			}
+
+			out = append(out, f)
+		}
+	}
+
+	if c != ';' && c != ')' {
+		return nil, errors.Parse(*pos, "Missing group end: %v", c)
+	}
+	
+	return forms.Group(fpos, out), nil
+}
+
+func (self TGroup) Form(pos *gofu.TPos, in *bufio.Reader) (gofu.Form, error) {
+	var c rune
+	var err error
 	fpos := *pos
 	
 	if c, _, err = in.ReadRune(); err == io.EOF {
@@ -34,32 +87,6 @@ func (self TGroup) Form(pos *gofu.TPos, in *bufio.Reader) (gofu.Form, error) {
 		return nil, nil
 	}
 
-	for {
-		f, err = self.memberParser.Form(pos, in)
-
-		if err != nil {
-			if err == io.EOF {
-				return nil, errors.Parse(*pos, "Missing group end")
-			}
-		}
-
-		if f == nil {
-			break
-		}
-
-		out = append(out, f)
-	}
-
-	if c, _, err = in.ReadRune(); err == io.EOF {
-		return nil, errors.Parse(*pos, "Missing group end")
-	} else if err != nil {
-		return nil, err
-	}
-		
-	if c != ')' {
-		return nil, errors.Parse(*pos, "Missing group end: %v", c)
-	}
-	
-	return forms.Group(fpos, out), nil
+	return self.Tail(fpos, pos, in)
 }
 
